53b55ceefb66889248dc8ac89e0fa6644422dba4
Anthony Rurup <tcrurup@gmail.com>
2019-04-30 22:07:16 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -8,8 +8,7 @@ class EmailParser
   attr_accessor :emails 
   
   def initialize(emails_list)
-    
-    self.emails = emails_list.split(/[, ]/)
+    self.emails = emails_list.split(/[,\s]/)
   end
   
   def parse
